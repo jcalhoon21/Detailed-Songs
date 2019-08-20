045ee48432e92c113dfaceebd203e28739331352
jcalhoon21 <jcalhoon21@gmail.com>
2019-08-20 14:19:20 -0700

    added first redux action file

--- web/src/components/App.jsx
+++ web/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+//import { selectSong } from '../actions'; // {} means youre exporting a named function ... VS no {} which would be an export default
 
 const App = () => {
   return <div>App</div>
