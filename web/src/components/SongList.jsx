5b33433e7f97d9a2a3e0337f60125ffe1cee2ea7
jcalhoon21 <jcalhoon21@gmail.com>
2019-08-20 15:13:01 -0700

    added ui to project displaying redux mapped data for the song list

--- web/src/components/SongList.jsx
+++ web/src/components/SongList.jsx
@@ -2,9 +2,36 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends React.Component {
+    renderList() {
+        return this.props.songs.map((song) => { //maps through an array of our song objects
+            return (
+                <div className='item' key={ song.title }>
+                    <div className='right floated content'>
+                        <button className='ui button primary'>
+                            SELECT
+                        </button>
+                    </div>
+                    <div className='content'>
+                        {song.title}
+                    </div>
+                </div>
+            );
+        });
+    }
+    
     render() {
-        return <div>SongList</div>
+        return <div className='ui divided list'>{this.renderList()}</div>
     }
 }
 
-export default connect()(SongList);
+// could be called anything it wants, by conevention its mapStateToProps
+// state contains all of the data inside the redux store
+const mapStateToProps = (state) => {
+    return { songs: state.songs }; // acts as props in the songlist component
+};
+
+export default connect(mapStateToProps)(SongList);
+
+//always going to have mapstatetoprops, that takes in state
+//always going to return in mapstatetoprops what will be considered props in the component
+//always going to have connect() with the current component (songlist)
